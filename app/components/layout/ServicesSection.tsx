157ac34da6486523d247f69ffa9c40447b4c438c
Pablo <pablo-krai@hotmail.com>
2025-08-13 20:56:55 -0300

    landing page done

---
+++ app/components/layout/ServicesSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ServiceCard from "@/app/components/layout/ServiceCard";
+
+const ServicesSection = () => {
+  const services = [
+    {
+      title: "Transferí dinero",
+      description:
+        "Desde Digital Money House vas a poder transferir dinero a otras cuentas, así como también recibir transferencias y nuclear tu capital en nuestra billetera virtual.",
+    },
+    {
+      title: "Pago de servicios",
+      description:
+        "Pagá mensualmente los servicios en 3 simples clicks. Fácil, rápido y conveniente. Olvidate de las facturas en papel.",
+    },
+  ];
+
+  return (
+    <section className="bg-accent w-full rounded-t-3xl px-4">
+      <div className="flex -translate-y-10 flex-col justify-center gap-4 xl:flex-row">
+        {services.map((card) => (
+          <ServiceCard
+            key={card.title}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ServicesSection;
